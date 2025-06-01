1edbaf63d947e7be005fd448f9944a884d2014d3
kawserkabir <kawserkabir123@gmail.com>
2025-06-01 18:37:22 +0600

    modify

--- factorial.java
+++ factorial.java
@@ -2,6 +2,12 @@ import java.util.Scanner;
 //factorial calculate input number 
 class funtions{
     public static void PrintFactorial(int n){
+        
+        if(n < 0){
+            System.err.println("INVALID NUMBER");
+            return;
+        }
+
         int factorial = 1;
 
         for (int i = n; i>=1; i--){
@@ -11,6 +17,7 @@ class funtions{
         System.out.println(factorial);
         return;
     }
+    
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
