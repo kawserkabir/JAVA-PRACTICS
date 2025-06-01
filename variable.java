9f62d72f36eaecb95cb4a93fd1a45ef531dd5c30
kawserkabir <kawserkabir123@gmail.com>
2025-06-01 08:35:13 +0600

    variable add

---
+++ variable.java
@@ -0,0 +1,11 @@
+class variable{
+    public static void main(String args[]){
+        //variable add a + b
+        int a = 10;
+        int b = 20;
+
+        int sum = a + b;
+
+        System.out.println(sum);
+    }
+}
