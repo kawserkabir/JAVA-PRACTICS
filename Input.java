b0bbe58e4b1ba5e4913baab8ea8916449ab4d5fd
kawserkabir <kawserkabir123@gmail.com>
2025-06-01 09:13:09 +0600

    input value and add

---
+++ Input.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Input{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("fast number");
+        int a = sc.nextInt();
+        System.out.println("2nd number");
+        int b = sc.nextInt();
+
+        System.out.println("sum");
+
+        int sum = a + b;
+
+        System.out.println(sum);
+    }
+}
