e2ebd34f421c263b773e9008162121ea581b2e24
kawserkabir <kawserkabir123@gmail.com>
2025-06-01 15:14:12 +0600

    print 0 to 10 using while loop

---
+++ whileloops.java
@@ -0,0 +1,9 @@
+class whileloops{
+    public static void main(String args[]){
+        int i = 0; //use while loop
+        while (i < 11){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
