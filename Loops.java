022de9580bcb1b63268c6d3075e091253ed7a4eb
kawserkabir <kawserkabir123@gmail.com>
2025-06-01 15:01:53 +0600

    loops print o to 10

---
+++ Loops.java
@@ -0,0 +1,8 @@
+class Loops{
+    public static void main(String args[]){
+//print 0 to 10 with loops
+        for(int i = 0; i < 11; i++){
+            System.out.println(i+" ");
+        }
+    }
+}
