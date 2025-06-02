e8b4bc08c9bfeb7a16aa44210715dbe507545d4f
kawserkabir <kawserkabir123@gmail.com>
2025-06-02 15:49:36 +0600

    cheak greatest numbr

---
+++ greatestnumbers.java
@@ -0,0 +1,23 @@
+class greatestnumbers{
+    public static void main(String args []){
+
+        int a = 5;
+        int b = 10;
+        
+        if (a > b) {
+            
+            System.out.println("a is greater");
+            
+        } else if (a < b) {
+            
+            System.out.println("b is greater");
+            
+        } else {
+            
+            System.out.println("Both are equal");
+        }
+
+
+    }
+
+}
