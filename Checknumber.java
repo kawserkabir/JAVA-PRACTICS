5834af1f07f57f40cda74fb5124ea50002d6ddd7
kawserkabir <kawserkabir123@gmail.com>
2025-06-02 15:32:36 +0600

    cheak number positive or negative

---
+++ Checknumber.java
@@ -0,0 +1,18 @@
+class Checknumber{
+
+    public static void main(String args []){
+
+        int num = -10;
+        
+        if (num > 0) {
+            System.out.println("Positive");
+        } else if (num < 0) {
+            System.out.println("Negative");
+        } else {
+            System.out.println("Zero");
+        }
+
+
+    }
+
+}
