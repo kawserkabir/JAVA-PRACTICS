062772dce68500b842a971aa7e7972203a990338
kawserkabir <kawserkabir123@gmail.com>
2025-06-03 14:14:45 +0600

    print charecters A to z

---
+++ PrintCharacters.java
@@ -0,0 +1,13 @@
+class PrintCharacters{
+    public static void main(String args []){
+
+        for (char ch = 'A'; ch <= 'Z'; ch++) {
+
+        System.out.print(ch + " ");
+
+    }
+
+
+    }
+
+}
