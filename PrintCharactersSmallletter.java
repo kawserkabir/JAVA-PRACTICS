27d9abd7c6ec98b7f09c9730dae1a39b3d3899ba
kawserkabir <kawserkabir123@gmail.com>
2025-06-17 17:46:28 +0600

    print small letter

---
+++ PrintCharactersSmallletter.java
@@ -0,0 +1,13 @@
+class PrintCharactersSmallLetter{
+    public static void main(String args []){
+          // print a to z 
+        for (char ch = 'a'; ch <= 'z'; ch++) {
+
+        System.out.print(ch + " ");
+
+    }
+
+
+    }
+
+}
