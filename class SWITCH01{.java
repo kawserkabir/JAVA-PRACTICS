2b59b71554c91e156e630b5e676d040d04efe2df
kawserkabir <kawserkabir123@gmail.com>
2025-06-25 09:02:46 +0600

    find day with switch case

---
+++ class SWITCH01{.java
@@ -0,0 +1,28 @@
+class SWITCH01{
+    public static void main(String args []){
+
+        int month = 5;
+        
+            switch (DAY) {
+            case 1 : System.out.println("SAT"); 
+            break;
+            case 2 : System.out.println("SUN"); 
+            break;
+            case 3 : System.out.println("MON"); 
+            break;
+            case 4 : System.out.println("TUE"); 
+            break;
+            case 5 : System.out.println("WED"); 
+            break;
+            case 6 : System.out.println("THU"); 
+            break;
+            case 7 : System.out.println("FRI"); 
+            break;
+             
+            default : System.out.println("Invalid DAY");
+        }
+
+
+    }
+
+}
