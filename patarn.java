9fb38c6099a8eb81d7c430aafe508304b4781e57
kawserkabir <kawserkabir123@gmail.com>
2025-07-22 15:36:38 +0600

    patterns

---
+++ patarn.java
@@ -0,0 +1,9 @@
+class patarn{
+    public static void main(String args[]){
+        System.out.println("*");
+        System.out.println("* *");
+        System.out.println("* * *");
+        System.out.println("* * * *");
+        System.out.println("* * * * *");
+    }
+}
