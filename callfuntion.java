271c1775df364836faff0596ad7ef3805433948d
kawserkabir <kawserkabir123@gmail.com>
2025-09-14 17:24:59 +0600

    call name

---
+++ callfuntion.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class kawser{
+    public static void myname(String name){
+        System.out.println(name);
+        return;
+    }
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+
+        myname(name);
+    }
+}
---
+++ kmoon.java
@@ -0,0 +1,8 @@
+class kmoon{
+    public static void main(String args[]){
+
+        for(int counter = 0; counter < 10; counter++){
+            System.out.println(counter+" ");
+        }
+    }
+}
