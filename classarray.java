2a923d4c0a36a23771fc413cb8707e716e6e3678
kawserkabir <kawserkabir123@gmail.com>
2025-09-15 09:26:21 +0600

    array

---
+++ classarray.java
@@ -0,0 +1,17 @@
+class array55{
+    public static void main(String args[]){
+        int marks[] = new int[3];
+        marks[0] = 33;
+        marks[1]= 44;
+        marks[2]= 55;
+
+       // System.out.println(marks[2]);
+       // System.out.println(marks[0]);
+       // System.out.println(marks[1]);
+        
+        for (int i = 0; i < 3; i++){
+            System.out.println(marks[i]);
+        }
+
+    }
+}
