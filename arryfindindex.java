1c47ebd76c5a7a544b8cd7342108b60b96a26ad4
kawserkabir <kawserkabir123@gmail.com>
2025-09-15 11:18:07 +0600

    find x value

---
+++ arryfindindex.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class kawser{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int marks[] = new int[size];
+
+        for (int i = 0; i<size; i++){
+            marks[i]= sc.nextInt();
+        }
+
+        int x = sc.nextInt();
+
+        for (int i = 0; i<size; i++){
+            if (marks[i] == x){
+                System.out.println("x found in index " +i);
+            }
+        }
+    }
+}
